704006ea85d3a6ff92505b40cd12b07a1a5f78f4
shareifjones <shareifjones@gmail.com>
2024-09-29 15:17:16 -0400

    fixing password

--- index.js
+++ index.js
@@ -319,11 +319,12 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), as
     return res.status(400).send('Permission denied');
   }
   // CONDITION ENDS
+  let hashedPassword = Users.hashPassword(req.body.Password);
   await Users.findOneAndUpdate({ Username: req.params.Username }, {
     $set:
     {
       Username: req.body.Username,
-      Password: req.body.Password,
+      Password: hashedPassword,
       Email: req.body.Email,
       Birthday: req.body.Birthday
     }
