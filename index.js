eeeed87e5b772c274c05bd822726cf5ba8dc7d7a
shareifjones <shareifjones@gmail.com>
2024-10-28 19:06:28 -0400

    commented out local server

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-/**  mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true }); */
+// mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
